b20aaf3a34490318f8eab0010e4accd43244e0da
jjw <1196557669>
2016-10-12 12:59:59 +0800

    new

---
+++ storm/storm03/src/main/java/bhz/topology/MessageTopology.java
@@ -0,0 +1,27 @@
+package bhz.topology;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import bhz.bolt.SpliterBolt;
+import bhz.bolt.WriterBolt;
+import bhz.spout.MessageSpout;
+
+public class MessageTopology {
+	
+	public static void main(String[] args) throws Exception {
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("spout", new MessageSpout());
+		builder.setBolt("split-bolt", new SpliterBolt()).shuffleGrouping("spout");
+		builder.setBolt("write-bolt", new WriterBolt()).shuffleGrouping("split-bolt");
+        //本地配置
+        Config config = new Config();
+        config.setDebug(false);
+        LocalCluster cluster = new LocalCluster();
+        System.out.println(cluster);
+        cluster.submitTopology("message", config, builder.createTopology());
+        Thread.sleep(10000);
+        cluster.killTopology("message");
+        cluster.shutdown();
+	}
+}
